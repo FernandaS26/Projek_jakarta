02be8ae93c6f084baecaefb466cf83234bd22c38
Ahmad Raihan Asyam <araihanassyam@gmail.com>
2024-03-06 15:22:06 +0800

    add DatabaseName.json and Update Print_nama.py

--- Print_nama.py
+++ Print_nama.py
@@ -1,1 +1,10 @@
-print ("Nama Kelompok Jakarta")
+import json
+
+with open('DatabaseName.json') as json_file:
+    data = json.load(json_file)
+
+name_list = data["name_list"]
+
+print ("Nama Anggota Kelompok Jakarta")
+for name in name_list:
+    print(name)
